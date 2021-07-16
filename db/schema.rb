ac5f69a65f18ff1c0711a10fcdef16deed92bf65
cohako <muri.ilo@hotmail.com>
2021-07-15 22:04:36 -0300

    Adicionado coluna active em suppliers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_16_004930) do
+ActiveRecord::Schema.define(version: 2021_07_16_010114) do
 
   create_table "items", force: :cascade do |t|
     t.string "code"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_07_16_004930) do
     t.string "trade_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "active"
     t.index ["cnpj"], name: "index_suppliers_on_cnpj", unique: true
   end
 
