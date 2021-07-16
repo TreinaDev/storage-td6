00aea8c00fc850ead09180b8962d46c63259ec43
cohako <muri.ilo@hotmail.com>
2021-07-15 22:16:08 -0300

    Adicionado default para coluna active para true

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_16_010114) do
+ActiveRecord::Schema.define(version: 2021_07_16_011016) do
 
   create_table "items", force: :cascade do |t|
     t.string "code"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_07_16_010114) do
     t.string "trade_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "active"
+    t.boolean "active", default: true
     t.index ["cnpj"], name: "index_suppliers_on_cnpj", unique: true
   end
 
