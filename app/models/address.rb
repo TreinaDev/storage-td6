56e7027d70babcf3c687d007e966c52265f7d9dd
Rogerio Bordignon <rog.bordignon@gmail.com>
2021-07-17 17:59:42 -0400

    Criando validações do model Address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :addressable, polymorphic: true
+
+  validates :name, :district, :city, :state, :zip_code, presence: true
+  validates :zip_code, numericality: { only_integer: true }
+  validates :zip_code, length: { is: 8 }
 end
