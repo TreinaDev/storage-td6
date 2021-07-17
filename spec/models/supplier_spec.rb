6c8a03f9c0b6f4654a0eb83c4683e145a0bfa20f
cohako <muri.ilo@hotmail.com>
2021-07-16 23:47:39 -0300

    Adicionado testes para validador de cnpj

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe Supplier, type: :model do
     it { should validate_presence_of(:trade_name) }
     it { should validate_uniqueness_of(:cnpj) }
   end
+  describe 'cnpj' do
+    it { should allow_value('29.317.821/0001-38').for(:cnpj) }
+    it { should_not allow_value('44.323.255/1233-41').for(:cnpj) }
+    it 'must be valid' do
+      invalid_cnpj = '29.212.453/0001-38'
+      should_not allow_value(invalid_cnpj).for(:cnpj)
+    end
+    it { should validate_length_of(:cnpj).is_equal_to(18) }
+  end
 end
