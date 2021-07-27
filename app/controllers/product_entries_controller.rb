8cf0398e514f664bd71b370e847dcd4604c97b85
cohako <muri.ilo@hotmail.com>
2021-07-27 19:13:21 -0300

    Reformulado os rescues

--- app/controllers/product_entries_controller.rb
+++ app/controllers/product_entries_controller.rb
@@ -1,4 +1,9 @@
 class ProductEntriesController < AuthenticationController
+  rescue_from MustBeAValidCsv, with: :redirect_to_new_csv_invalid
+  rescue_from FileMustBeUploaded, with: :redirect_to_new_file_not_uploaded
+
+  before_action :file_uploaded?, only: %i[create_by_csv]
+
   def index
     @product_entry = ProductEntry.all
   end
@@ -22,8 +27,6 @@ class ProductEntriesController < AuthenticationController
   end
 
   def create_by_csv
-    raise FileMustBeUploaded if params[:product_entry].blank?
-
     if ProductEntry.import_from_csv(params[:product_entry][:csv], current_user.warehouse)
       redirect_to product_entries_path, notice: t('.success')
     else
@@ -35,5 +38,17 @@ class ProductEntriesController < AuthenticationController
 
   def entry_params
     params.require(:product_entry).permit(:sku, :invoice, :quantity, :supplier_id)
+  end
+
+  def file_uploaded?
+    raise FileMustBeUploaded if params.dig(:product_entry, :csv).blank?
+  end
+
+  def redirect_to_new_file_not_uploaded
+    redirect_to new_product_entry_path, notice: t('.file_must_be_uploaded')
+  end
+
+  def redirect_to_new_csv_invalid
+    redirect_to new_product_entry_path, notice: t('.csv_must_be_valid')
   end
 end
