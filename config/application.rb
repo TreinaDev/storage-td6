a39ba9f211fedf6a16328db64ebc0bf0735edca9
Laryssa_LFA <lfadarkangel@gmail.com>
2021-07-29 13:16:52 -0300

    Estilizando páginas de entrada de produto

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# you've limited to :test, :development, or :production./
 Bundler.require(*Rails.groups)
 
 module Storage
